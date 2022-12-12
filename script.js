df60be3111980c8615ab69e1511a6cab6e9001a3
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2022-12-13 00:38:18 +0100

    Created repo and notes from first week

---
+++ script.js
@@ -0,0 +1,51 @@
+/*
+console.log('Hello World!')
+let age = 23;
+console.log(age);
+*/
+
+/*
+    Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+    typeof ile datamızın tipini öğreniriz.
+console.log(typeof javascriptIsFun);
+console.log(typeof true);  //boolean
+console.log(typeof 23);  // number
+console.log(typeof 'Yunus');  //string
+
+javascriptIsFun = "YES!";  // let ile yazılan biri valueyi daha sonra let yazmadan yeni bir value olarak değştirebilirsin.
+console.log(javascriptIsFun);
+
+let x;
+console.log(typeof x); //undefined type
+console.log(typeof null); // object type!!!!--be carefull this one
+
+
+        //Basic Operators
+let now = 2022;
+const ageYunus = now - 1992;
+const ageKerime= now - 1996;
+console.log(ageYunus, ageKerime);
+console.log(ageYunus * 2, ageKerime /2, 3 ** 4)
+// 3 ** 4 => bu işlem 3 üzeri 4 tür
+
+let firstName = 'Yunus';
+let lastName = 'Beyaz';
+console.log(firstName + " " + lastName);
+        // Assingment Operators
+let x = 12 + 7; // x =19
+x += 10  // x = x + 10 = 29
+x *= 2  // x = x * 2 = 58
+x ++ // x = x + 1  = 59
+x -- // x = x -1 = 58
+console.log(x);
+
+        //Comparison Operators
+console.log(ageYunus > ageKerime); //true >, <, =, >=, <=
+console.log(ageKerime >= 18); //true
+console.log(ageYunus <= 28); //false
+
+*/
