ce4e03765af9d116dd1f27559bbafda87904b72f
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2022-12-16 22:26:00 +0100

    made some practice on if/else statement

--- script.js
+++ script.js
@@ -48,7 +48,21 @@ console.log(ageYunus > ageKerime); //true >, <, =, >=, <=
 console.log(ageKerime >= 18); //true
 console.log(ageYunus <= 28); //false
 
+// "&& eşittir operatörü. ikisi de aynı ise true sonucu verir
+// "|| or operatörü. ikisinden biri ture isi true verir. ikiside false is false verir. ikisi de ture ise true verir.
+// "!" ise değilse yanlışsa anlamı taşır !true dersek false cevabını arıyoruzdur.
 
+let goodAtCSS = true;
+let goodAtJavascript = false;
+
+let goodAtFindJob = goodAtJavascript && goodAtCSS
+
+console.log(goodAtFindJob)
+
+
+let refused = !goodAtFindJob;
+
+console.log(refused)
 
 let x,y;  //we created 2 variable in 1 line
 x = y = 25 - 10 - 5;
@@ -141,7 +155,7 @@ let markLoverBMI = false;
 let message = speed >= 120 ? 'Too Fast' : speed >= 80 ? 'Fast' : 'OK';
 
 */
-
+/*
 let weightOfMark = 78;
 const heightOfMark = 1.69;
 
@@ -150,7 +164,7 @@ const heightOfJohn= 1.95;
 
 let bmiOfMark = weightOfMark / (heightOfMark ** 2);
 let bmiOfJohn = weightOfJohn / (heightOfJohn * heightOfJohn);
-/*
+
 const markHigherBMI = bmiOfMark > bmiOfJohn;
 console.log(markHigherBMI);
  
@@ -160,7 +174,7 @@ let markLoverBMI = false;
         console.log(markHigherBMI)
  } else {
         console.log(markLoverBMI)
- };  */ 
+ };  
 
 
  if (bmiOfMark > bmiOfJohn){
@@ -168,3 +182,100 @@ let markLoverBMI = false;
  } else {
        console.log("Mark's BMI is lower than Jhon's")
  }
+ */ 
+
+
+       //TYPE CONVERTION AND COERTION:
+
+      // bize genelde veriler string olarak gelecektir. bu nedenle hesaplama yapmak istediğimizde hata alırız. burada value değiştirme ve değiştirmeye zorlamayı öğreneceğiz.
+/*
+const birthYear = '1992';
+//typeof birthyear is =String
+//if we want to calculate as a number we should use Number(birthYear) function.
+console.log(Number(birthYear) + 18); //= 2010
+let yunus = 25
+console.log(String(yunus));
+// We can convert to a number, string or boolean. Only 3 types.
+
+        coertion is very different :
+       When we use + like this;
+       console.log('I am ' + 30 + ' years old.')  => javascript forces 23 to be a string and we see I am 30 years old.I
+       console.log('30' - 5 - 12)  => SHOWS US 13 . javascript forces strings to be Number. but be carefull. If you use + it wont change to number !!!!
+       console.log('30' + 5 + '10') => shows us 30510 !!!!!
+       + ile numarayı stringe çevirir.
+       - ile stringi numaraya çevirir.
+
+       let n = 2 + 3 + 5 + '5'; // shows us 105
+       console.log(n);
+
+       Boolean convertion is little different:
+       Only 0 = number zero, '' = empty string , undefined, null and NAN şs false. all others are true.
+       console.log(Boolean(0));  //false
+       console.log(Boolean('')); //false
+       console.log(Boolean('yunus'));  //true
+       console.log(Boolean({}));  //true
+
+
+      //  PROMPT bu bir form çeşididir sayfanın başında alert gibi ekran gelir ve bilgi girilebilir. console de sonuç gözükür.
+
+const favouriteNumber = prompt("What is your favourite number?");
+
+if(favouriteNumber === 55){
+       console.log("That's a GREAT number!")
+}
+
+//bu şekilde kullanıcı 55 yazarsa sonuç alamaz çünkü girdiği zaman javacript bunu string olarak algılar. bunu çözmek için prompun başıa number function getirmelisin
+
+const favouriteNumber = Number(prompt("What is your favourite number?"));
+if(favouriteNumber === 55){
+       console.log("That's a GREAT number!")
+} else if (favouriteNumber === 12){
+       console.log("12 is a great number too!")
+} else if (favouriteNumber === 45){
+       console.log("45 is not the coolest number")
+} else {
+       console.log('You choosed wrong number.')
+};
+
+  //     && and , || or , ! not
+const yunusIsGoodDeveloper = true;
+const yunusHasResistentPermit = false;
+console.log(yunusIsGoodDeveloper && yunusHasResistentPermit); //false
+console.log(yunusIsGoodDeveloper || yunusHasResistentPermit); //true
+console.log(!yunusIsGoodDeveloper); //false
+
+const celebrateNewYear = yunusIsGoodDeveloper && yunusHasResistentPermit;
+if (celebrateNewYear){
+       console.log("Happy New Year!!!")
+} else {
+       console.log("Just keep working, you can celebrate in 2024 :(")
+};
+
+
+const dolphins = 96 + 108 + 89;
+const koalas = 88 + 91 + 110;
+const winnerTeam = dolphins - koalas;
+
+if (winnerTeam > 0){
+       console.log("The winner is Dolphins!")
+} else if(winnerTeam < 0){
+       console.log("The winner is Koalas!")
+} else {
+       console.log("There is no winner!")
+};
+*/
+
+const dolphins = (97 + 112 + 1) / 3;
+const koalas = (109 + 95 + 1) / 3;
+console.log(dolphins, koalas);
+
+if (dolphins > koalas && dolphins >= 100){
+       console.log("Dolphins is the winner!");
+} else if(koalas > dolphins && koalas >= 100) {
+       console.log("Koalas is the winner!");
+} else if (dolphins === koalas && dolphins >= 100 && koalas >= 100){
+       console.log("There is no winner!");
+} else {
+       console.log("Both team are LOOSERS!")
+}
+
